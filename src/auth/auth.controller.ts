ff639fe95bf1bf7e6249d33ff074a1cd741138c6
gabriel-g4 <gbgarcia642@gmail.com>
2025-06-28 17:52:22 -0300

    feat: swagger ui e imagenes de perfil

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { extname } from 'path';
+import { FileValidationPipe } from 'src/common/pipes/file-validation.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -12,8 +16,25 @@ export class AuthController {
     return this.authService.login(loginDto);
   }
 
-  @Post('register')
-  register(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto);
-  }
+   @Post('register')
+    @UseInterceptors(
+      FileInterceptor('imagen', {
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (req, file, cb) => {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+            const ext = extname(file.originalname);
+            cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+          },
+        }),
+      }),
+    )
+    async register(
+      @Body() registerDto: RegisterDto,
+      @UploadedFile(FileValidationPipe) file: Express.Multer.File,
+    ) {
+      const imagenPerfilUrl = file ? `/uploads/${file.filename}` : undefined;
+
+      return this.authService.register(registerDto, imagenPerfilUrl);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -22,6 +23,16 @@ async function bootstrap() {
   // Habilitar CORS
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Red Social') // Título de la documentación
+    .setDescription('API para una red social con usuarios, posts y comentarios.') // Descripción opcional
+    .setVersion('1.0') // Versión de la API
+    .addBearerAuth() // Si usas autenticación JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Accede a la documentación en /api
   
   let puerto = process.env.PORT ?? 3000
   await app.listen(puerto);
